d11e0c329e7c9965413f93e1ab307d7f46361a54
Xswinger <68491440+Xswinger@users.noreply.github.com>
2023-12-22 21:16:54 +0300

    added in modules bootstrap icons, create home, coach, rating, training pages, moved profile in content folder, change icons in header

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,15 +2,22 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SportsmanComponent } from './content/sportsman/sportsman.component';
 import { SportsSchoolComponent } from './content/sports-school/sports-school.component';
-import { ProfileComponent } from './profile/profile.component';
+import { ProfileComponent } from './content/profile/profile.component';
 import { CompetitionComponent } from './content/competition/competition.component';
+import { RatingComponent } from './content/rating/rating.component';
+import { HomeComponent } from './content/home/home.component';
+import { CoachComponent } from './content/coach/coach.component';
+import { TrainingComponent } from './content/training/training.component';
 
 const routes: Routes = [
   { path: "sportsman", component: SportsmanComponent },
   { path: "sports_school", component: SportsSchoolComponent },
   { path: "profile", component: ProfileComponent },
   { path: "competition", component: CompetitionComponent },
-  { path: "**", redirectTo: "/" } 
+  { path: "rating", component: RatingComponent },
+  { path: "coach", component: CoachComponent },
+  { path: "training", component: TrainingComponent },
+  { path: "**", component: HomeComponent } 
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,14 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { SportsSchoolComponent } from './content/sports-school/sports-school.component';
 import { SportsmanComponent } from './content/sportsman/sportsman.component';
-import { ProfileComponent } from './profile/profile.component';
+import { ProfileComponent } from './content/profile/profile.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CompetitionComponent } from './content/competition/competition.component';
+import { RatingComponent } from './content/rating/rating.component';
+import { MatIconModule } from '@angular/material/icon';
+import { HomeComponent } from './content/home/home.component';
+import { CoachComponent } from './content/coach/coach.component';
+import { TrainingComponent } from './content/training/training.component';
 
 @NgModule({
   declarations: [
@@ -17,12 +22,17 @@ import { CompetitionComponent } from './content/competition/competition.componen
     SportsSchoolComponent,
     SportsmanComponent,
     ProfileComponent,
-    CompetitionComponent
+    CompetitionComponent,
+    RatingComponent,
+    HomeComponent,
+    CoachComponent,
+    TrainingComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
